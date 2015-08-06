3c201eca763ad10a8e2aa2c6969f1739391aff32
pallavis-webonise <pallavi.srikhakollu@gmail.com>
2015-08-06 11:05:54 +0530

    resubmitting assignment one with required changes

--- Assignment1/Assignment1/CalculateSimpleInterest.m
+++ Assignment1/Assignment1/CalculateSimpleInterest.m
@@ -11,7 +11,7 @@
 
 float const simpleInterestDivisor=100;
 
-+(float)simpleInterestCalculatorWithAmount:(float)principalAmount withinterest:(float)rate withtime:(float)timePeriod  {
+-(float)simpleInterestCalculatorWithAmount:(float)principalAmount withinterest:(float)rate withtime:(float)timePeriod  {
     
     float amount=(principalAmount*rate*timePeriod)/(simpleInterestDivisor);
     return amount;
